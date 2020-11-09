e557ff9396e2c35c55bba5315c00f90ee2775cc8
moyuanhua <moyuanhua>
2020-11-09 14:34:41 +0800

    'feat:amqp'

---
+++ src/Amqp/BaseConsumer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Amqp\Consumer;
+
+use Hyperf\Amqp\Annotation\Consumer;
+use Hyperf\Amqp\Message\ConsumerMessage;
+use PhpAmqpLib\Message\AMQPMessage;
+use Hyperf\Amqp\Result;
+
+/**
+ * @Consumer(exchange="hyperf", routingKey="hyperf", name ="BaseConsumer", nums=1, enable=false)
+ */
+class BaseConsumer extends ConsumerMessage
+{
+    public function getQueue(): string
+    {
+        return $this->exchange.'.'.$this->routingKey;
+    }
+
+    public function consumeMessage($data, AMQPMessage $message): string
+    {
+        print_r($data);
+
+        return Result::ACK;
+    }
+}
---
+++ src/Amqp/WebSocketProducer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Amqp\Producer;
+
+use Hyperf\Amqp\Annotation\Producer;
+use Hyperf\Amqp\Message\ProducerMessage;
+
+/**
+ * @Producer(exchange="websocket", routingKey="")
+ */
+class WebSocketProducer extends ProducerMessage
+{
+
+    /**
+     * WebSocketProducer constructor.
+     * @param string $ns 用户的命名空间 Admin: fhs/admin User: fhs/user
+     * @param string $uid 用户的id
+     * @param string $event 定义的websocket事件
+     * @param array $data 数据
+     */
+    public function __construct(string $ns,string $uid='',string $event,array $data)
+    {
+        $this->payload = $data;
+    }
+
+}
