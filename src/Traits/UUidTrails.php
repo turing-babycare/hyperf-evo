e943b5eb71a75a4b871057c1f0e45e89e3714103
moyuanhua <moyuanhua>
2020-11-10 09:31:37 +0800

    'feat:uuid'

--- src/Traits/UUidTrails.php
+++ src/Traits/UUidTrails.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Turing\HyperfEvo\Traits;
 
 use Hyperf\Snowflake\IdGeneratorInterface;
+use Psr\Container\ContainerInterface;
 
 /**
  * Trait UUidTrails
@@ -13,10 +14,14 @@ use Hyperf\Snowflake\IdGeneratorInterface;
 trait UUidTrails
 {
     /**
-     * @inject
      * @var IdGeneratorInterface
      */
     private  IdGeneratorInterface $idGenerator;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->idGenerator = $container->get(IdGeneratorInterface::class);
+    }
 
     /**
      * 获取全局唯一id 基于 https://hyperf.wiki/2.0/#/zh-cn/snowflake
