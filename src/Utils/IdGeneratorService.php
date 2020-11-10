44b7c73c83c7b25e33b201c65b92eb0ed6310733
moyuanhua <moyuanhua>
2020-11-10 17:23:20 +0800

    'feat:uuid-service'

---
+++ src/Utils/IdGeneratorService.php
@@ -0,0 +1,33 @@
+<?php
+
+
+declare(strict_types=1);
+namespace Turing\HyperfEvo\Utils;
+
+
+use Hyperf\Snowflake\IdGeneratorInterface;
+
+class IdGeneratorService
+{
+    /**
+     * @inject
+     * @var IdGeneratorInterface
+     */
+    protected IdGeneratorInterface $idGenerator;
+
+    /**
+     * 获取全局唯一id 基于 https://hyperf.wiki/2.0/#/zh-cn/snowflake
+     * @return int
+     */
+    public function getUuid(){
+        return $this->idGenerator->generate();
+    }
+
+    /**
+     * @param int $id
+     * @return \Hyperf\Snowflake\Meta
+     */
+    public function getMeta(int $id){
+        return $this->idGenerator->degenerate($id);
+    }
+}
