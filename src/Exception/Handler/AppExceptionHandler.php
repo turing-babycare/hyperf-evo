6c98fb67e7e105e10eb7c39ab6e61abf7d7b6f37
WenLei <jasonwx@163.com>
2020-12-23 15:27:03 +0800

    improve error logs

--- src/Exception/Handler/AppExceptionHandler.php
+++ src/Exception/Handler/AppExceptionHandler.php
@@ -43,11 +43,11 @@ class AppExceptionHandler extends ExceptionHandler
 
         $responseBody = [
             'trace_id' => $traceId,
+            'service_name' => env('APP_NAME'),
             'message' => $throwable->getMessage(),
             'stack' => $stack
         ];
-
-
+        $errorDetail = [];
         if ($throwable instanceof NotFoundHttpException) {
             $responseBody['message'] = '接口不存在.';
             $response = $this->response->json($responseBody)->withStatus(404);
@@ -58,8 +58,22 @@ class AppExceptionHandler extends ExceptionHandler
             $response = $this->response->json($responseBody)->withStatus(400);
         } else if ($throwable instanceof ServiceClientException) {
             $statusCode = 500;
+            if ($throwable->request) {
+                $uri = $throwable->request->getUri();
+                $errorDetail["request"] = [
+                    'method' => $throwable->request->getMethod(),
+                    'url' => "{$uri->getScheme()}://{$uri->getAuthority()}{$uri->getPath()}?{$uri->getQuery()}",
+                    'headers' => $throwable->request->getHeaders(),
+                    'body' => $throwable->request->getBody()->getContents()
+                ];
+            }
             if ($throwable->response) {
                 $statusCode = $throwable->response->getStatusCode();
+                $errorDetail["response"] = [
+                    'headers' => $throwable->response->getHeaders(),
+                    'status' => $throwable->response->getStatusCode(),
+                    'body' => $throwable->response->getBody()->getContents()
+                ];
             }
             $response = $this->response->json($responseBody)->withStatus($statusCode);
         } else {
@@ -67,8 +81,10 @@ class AppExceptionHandler extends ExceptionHandler
             $response = $this->response->json($responseBody)->withStatus(500);
         }
 
+        $split = env('APP_ENV') === 'dev' ? "\n" : ' ';
+        $errDetailStr = json_encode($errorDetail, env('APP_ENV') === 'dev' ? JSON_PRETTY_PRINT : null);
+        $this->logger->error("[RESPONSE-ERROR]{$split}SERVICE_NAME: {$responseBody['service_name']}{$split}TRACE_ID: {$traceId}{$split}message: {$throwable->getMessage()}{$split}stack: {$throwable}{$split}detail: {$errDetailStr}");
 
-        $this->logger->error($stack);
         return $response;
     }
 
